446c93bc4efdd1c6edd82df373b52ca97dda1344
BrenLau <blau4000@gmail.com>
2023-05-17 16:04:26 -0400

    image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import CreateEmail from './components/CreateEmail/CreateEmail';
 
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+
 function App() {
+
   return (
     <div className="App">
+      <img className='background' src={images['image1.jpg']}></img>
       <CreateEmail></CreateEmail>
 
     </div>
