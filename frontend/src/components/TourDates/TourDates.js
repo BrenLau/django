98eb2c728c1f90204aeee9fb72178a30b658c17e
BrenLau <blau4000@gmail.com>
2023-07-12 12:36:20 -0400

    added cities

--- frontend/src/components/TourDates/TourDates.js
+++ frontend/src/components/TourDates/TourDates.js
@@ -1,8 +1,19 @@
 import "./tourdates.css"
 
 const TourDates = () => {
+
     return (
-        <div className="tourdatesdiv"></div>
+        <div className="tourdatesdiv">
+            <div className="tourcity">
+                <div>Tour 1</div>
+            </div>
+            <div className="tourcity">
+                <div>Tour 1</div>
+            </div>
+            <div className="tourcity">
+                <div>Tour 1</div>
+            </div>
+        </div>
     )
 }
 
